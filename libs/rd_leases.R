ef8d2301a9804232f3c086ef4fb494c79c5f0e0c
bchua-pm <ckc4148>
2020-09-06 22:47:30 -0400

    init

---
+++ libs/rd_leases.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(tidyr)
+library(purrr)
+#-------------------------------------------------------------------------------------------------
+rd.leases.adjustments.f <- function(financial_data, cost_debt=0.04){
+}
+#-------------------------------------------------------------------------------------------------
+# rm(rd.leases.adjustments.f)
